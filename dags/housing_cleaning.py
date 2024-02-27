c410b1a2909be70981b53ca371e699eb1bd07732
jujaemin <147119777+jujaemin@users.noreply.github.com>
2024-02-27 19:19:12 +0900

    fix: 경로수정

--- dags/housing_cleaning.py
+++ dags/housing_cleaning.py
@@ -21,7 +21,7 @@ def cleaning(**context):
 
         print(data)
 
-        file_path = f'/works/'
+        file_path = '/temp/'
         file_name = '{}.csv'.format(execution_date)
         local = file_path+file_name
 
