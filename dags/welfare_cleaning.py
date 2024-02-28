71de496e5d00bbecd935fbb0cc52d9d3d525e042
jujaemin <147119777+jujaemin@users.noreply.github.com>
2024-02-28 17:46:08 +0900

    Add files via upload

---
+++ dags/welfare_cleaning.py
@@ -0,0 +1,52 @@
+from plugins.cleaning import Cleaning
+from airflow import DAG
+from datetime import timedelta
+from airflow.decorators import task
+from plugins import filter
+from plugins.utils import FileManager
+from plugins.s3 import S3Helper
+
+import datetime
+
+
+@task
+def cleaning(**context):
+    try:
+        execution_date = context['execution_date'].date()
+        data = Cleaning.read_csv_to_df('welfare', execution_date, filter.column_indexes['welfare'])
+        data = Cleaning.check_pk_validation(Cleaning.rename_cols(data, 'welfare'), '자치구' if '자치구' in filter.columns['noise'] else '권역')
+        result_data = Cleaning.unify_null(data)
+
+        result_data = Cleaning.filter(result_data, 'welfare')
+
+
+        file_path = '/works/'
+        file_name = '{}.csv'.format(execution_date)
+        local = file_path+file_name
+
+        s3_key = 'cleaned_data/seoul_welfare/' + file_name
+
+        result_data.to_csv(local, header = False, index = False, encoding='utf-8-sig')
+
+        S3Helper.upload(aws_conn_id, bucket_name, s3_key, local, True)
+
+        FileManager.remove(local)
+    
+    except:
+        pass
+
+with DAG(
+    dag_id = 'Welfare_Cleaning',
+    start_date = datetime.datetime(2024,1,1),
+    schedule = '@daily',
+    max_active_runs = 1,
+    catchup = True,
+    default_args = {
+        'retries': 1,
+        'retry_delay': timedelta(minutes=1),
+    }
+) as dag:
+    aws_conn_id='aws_default'
+    bucket_name = 'de-team5-s3-01'
+
+    cleaning()
