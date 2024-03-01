34458f07abddb21f3be886e81a091cc60ee37c9f
jujaemin <147119777+jujaemin@users.noreply.github.com>
2024-03-01 11:46:55 +0900

    fix: parquet 파일로 저장

--- dags/housing_cleaning.py
+++ dags/housing_cleaning.py
@@ -10,41 +10,50 @@ import datetime
 
 
 @task
-def cleaning(**context):
-    try:
-        execution_date = context['execution_date'].date()
-        data = Cleaning.read_csv_to_df('housing', execution_date, filter.column_indexes['housing'])
-        data = Cleaning.check_pk_validation(Cleaning.rename_cols(data, 'housing'), '자치구' if '자치구' in filter.columns['housing'] else '권역')
-        result_data = Cleaning.unify_null(data)
+def cleaning():
+    dates = []
+    start_date = datetime.datetime(2024,1,1).date()
+    end_date = datetime.datetime.today().date()
+    current_date = start_date
 
-        result_data = Cleaning.filter(result_data, 'housing')
+    while current_date <= end_date:
+        dates.append(current_date)
+        current_date += timedelta(days=1)
 
-        print(data)
+    for date in dates:
+        try:
+            execution_date = date
+            data = Cleaning.read_csv_to_df('housing', execution_date, filter.column_indexes['housing'])
+            data = Cleaning.check_pk_validation(Cleaning.rename_cols(data, 'housing'), '자치구' if '자치구' in filter.columns['housing'] else '권역')
+            result_data = Cleaning.unify_null(data)
 
-        file_path = 'temp/seoul_housing/'
-        file_name = '{}.csv'.format(execution_date)
+            result_data = Cleaning.filter(result_data, 'housing')
+            result_data['건물명'] = result_data.apply(lambda row: 'NULL' if row['건물용도'] == '단독다가구' else row['건물명'], axis=1)
 
-        FileManager.mkdir(file_path)
-        
-        path = file_path+file_name
 
-        s3_key = 'cleaned_data/seoul_housing/' + file_name
+            save_path = 'temp/seoul_air/housing/'
+            file_name = f'{execution_date}.parquet'
+            path = save_path+file_name
 
-        result_data.to_csv(path, header = False, index = False, encoding='utf-8-sig')
+            FileManager.mkdir(save_path)
 
-        S3Helper.upload(aws_conn_id, bucket_name, s3_key, path, True)
+            result_data.to_parquet(path, index=False)
 
-        FileManager.remove(path)
+            s3_key = 'cleaned_data/seoul_housing/' + file_name
+
+            S3Helper.upload(aws_conn_id, bucket_name, s3_key, path, True)
+
+            FileManager.remove(path)
     
-    except:
-        pass
+        except:
+            pass
 
 with DAG(
-    dag_id = 'Housing_Cleaning',
+    dag_id = 'Housing_Cleaning109199392546',
     start_date = datetime.datetime(2024,1,1),
     schedule = '@daily',
     max_active_runs = 1,
-    catchup = True,
+    catchup = False,
     default_args = {
         'retries': 1,
         'retry_delay': timedelta(minutes=1),
