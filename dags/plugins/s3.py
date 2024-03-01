51660a1633b37be8823e86d347fa60513ec19a15
jujaemin <147119777+jujaemin@users.noreply.github.com>
2024-03-01 11:32:18 +0900

    fix: 인자수정

--- dags/plugins/s3.py
+++ dags/plugins/s3.py
@@ -4,7 +4,7 @@ from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 class S3Helper(AirflowPlugin):
     #Upload files to S3
     @classmethod
-    def upload(s3_conn_id: str, bucket_name: str, key: str, filename: str, replace: bool):
+    def upload(cls, s3_conn_id: str, bucket_name: str, key: str, filename: str, replace: bool):
         s3_hook = S3Hook(s3_conn_id)
         s3_hook.load_file(
             filename=filename,
