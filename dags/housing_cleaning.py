ded0b2549f79192dcfd075dc10ea70b28decf8ba
jujaemin <147119777+jujaemin@users.noreply.github.com>
2024-03-02 09:42:40 +0900

    fix: externaltasksensor 활용 trigger 작업

--- dags/housing_cleaning.py
+++ dags/housing_cleaning.py
@@ -64,7 +64,7 @@ with DAG(
     
     sensor = ExternalTaskSensor(
         task_id='externaltasksensor',
-        external_dag_id='Seoul_housing9',
+        external_dag_id='Seoul_Housing',
         external_task_id='upload',
         timeout=5*60,
         mode='reschedule'
