5054e9ed5c8ad898c3fb44f0a5a638783d5a75ae
jujaemin <147119777+jujaemin@users.noreply.github.com>
2024-03-02 09:56:28 +0900

    fix: external dag id 수정

--- dags/noise_cleaning.py
+++ dags/noise_cleaning.py
@@ -54,7 +54,7 @@ with DAG(
 
     sensor = ExternalTaskSensor(
         task_id='externaltasksensor',
-        external_dag_id='seoul_noise',
+        external_dag_id='etl_seoul_noise',
         external_task_id='load',
         timeout=5*60,
         mode='reschedule'
