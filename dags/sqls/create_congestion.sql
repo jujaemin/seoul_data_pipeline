8a41a41b005a5123f380fec1215875052c5561d0
cbs02112 <cbs02112@gmail.com>
2024-03-05 19:29:14 +0900

    DF-65 feat: unscheduled ELT 통합

--- dags/sqls/create_congestion.sql
+++ dags/sqls/create_congestion.sql
@@ -1,16 +1,10 @@
 CREATE TABLE {{ params.output_table_name }} AS
-SELECT
-    h_n.gu AS gu,
-    ((h_n.subtotal * h_b.subtotal * 0.4) + (s_n.subtotal * s_a.subtotal * 0.3) + p_r.ratio * 0.3) / 10000 AS index
-FROM
-    hospital_num_per_yr AS h_n
-JOIN
-    hospital_bed_per_yr AS h_b ON h_n.gu = h_b.gu AND h_n.year = h_b.year
-JOIN
-    sports_num_per_yr AS s_n ON h_b.gu = s_n.gu AND h_b.year = s_n.year
-JOIN
-    sports_area_per_yr AS s_a ON s_n.gu = s_a.gu AND s_n.year = s_a.year
-JOIN
-    park_ratio_per_yr AS p_r ON s_a.gu = p_r.gu AND s_a.year = p_r.gu.year
-WHERE
-    h_n.year = 2022
+SELECT 
+    division_name,
+	COUNT(
+		CASE
+			WHEN avg_speed < 23 THEN 1 ELSE NULL
+		END
+	) / AVG(avg_speed) AS calculated
+FROM '{{ params.source_database }}'.'seoul_road'
+GROUP BY division_name
--- dags/sqls/create_welfare.sql
+++ dags/sqls/create_welfare.sql
@@ -1,16 +1,16 @@
 CREATE TABLE {{ params.output_table_name }} AS
 SELECT
-    h_n.gu AS gu,
-    ((h_n.subtotal * h_b.subtotal * 0.4) + (s_n.subtotal * s_a.subtotal * 0.3) + p_r.ratio * 0.3) / 10000 AS index
+    m_n.gu AS gu,
+    ((m_n.subtotal * m_b.subtotal * 0.4) + (s_n.subtotal * s_a.subtotal * 0.3) + p.ratio * 0.3 * 1000) / 10000 AS index
 FROM
-    hospital_num_per_yr AS h_n
+    medical_num AS m_n
 JOIN
-    hospital_bed_per_yr AS h_b ON h_n.gu = h_b.gu AND h_n.year = h_b.year
+    medical_bed AS m_b ON m_n.gu = m_b.gu AND m_n.year = m_b.year
 JOIN
-    sports_num_per_yr AS s_n ON h_b.gu = s_n.gu AND h_b.year = s_n.year
+    sports_num AS s_n ON m_b.gu = s_n.gu AND m_b.year = s_n.year
 JOIN
-    sports_area_per_yr AS s_a ON s_n.gu = s_a.gu AND s_n.year = s_a.year
+    sports_area AS s_a ON s_n.gu = s_a.gu AND s_n.year = s_a.year
 JOIN
-    park_ratio_per_yr AS p_r ON s_a.gu = p_r.gu AND s_a.year = p_r.gu.year
+    raw_data.seoul_park AS p ON s_a.gu = p.gu AND s_a.year = p.year
 WHERE
-    h_n.year = 2022
+    m_n.year = 2022
