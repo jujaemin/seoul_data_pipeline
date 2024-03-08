d553b195d8a8907882174f6c0374089e9145406b
김동연 <yoyo0704@naver.com>
2024-03-08 14:27:14 +0900

    Update housing_price_per_sqm_stats.sql

--- dags/sqls/housing_price_per_sqm_stats.sql
+++ dags/sqls/housing_price_per_sqm_stats.sql
@@ -1,22 +1,19 @@
-CREATE TABLE {{ params.output_database }}.{{ params.output_table }} AS (
-    WITH price_per_sqm AS (
-    SELECT
-        DATE_FORMAT(contracted_date, '%Y-%m') AS year_month,
-        gu,
-        CAST(ROUND(price / house_area) AS INTEGER) AS price_per_sqm
-    FROM
-        "raw_data"."seoul_housing"
-    )
-    SELECT
-        year_month,
-        gu,
-        MIN(price_per_sqm) as min_price_per_sqm,
-        MAX(price_per_sqm) as max_price_per_sqm,
-        CAST(ROUND(AVG(price_per_sqm)) AS INTEGER) as avg_price_per_sqm
-    FROM
-        price_per_sqm
-    GROUP BY
-        1,2
-    ORDER BY
-        1,5
+CREATE TABLE {{ params.output_database }}.{{ params.output_table }} AS(
+WITH price_sqm_stats AS (
+SELECT
+    DATE_FORMAT(contracted_date, '%Y-%m') AS year_month,
+    gu,
+    price / house_area AS price_per_sqm
+FROM
+    "raw_data"."seoul_housing"
+)
+SELECT
+  year_month,
+  gu,
+  COUNT(*) AS trading_volume,
+  AVG(price_per_sqm) * 3.306 as avg_price_per_pyu
+FROM
+  price_sqm_stats
+GROUP BY
+  1,2
 )
