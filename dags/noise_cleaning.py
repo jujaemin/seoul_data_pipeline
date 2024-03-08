4bb233d140d55627c8b9d408797123b0bc49fc1e
jujaemin <147119777+jujaemin@users.noreply.github.com>
2024-03-08 14:08:07 +0900

    style: 대그명 수정

--- dags/noise_cleaning.py
+++ dags/noise_cleaning.py
@@ -57,7 +57,7 @@ with DAG(
 
     sensor = ExternalTaskSensor(
         task_id='externaltasksensor',
-        external_dag_id='etl_seoul_noise',
+        external_dag_id='ETL_seoul_noise',
         external_task_id='load',
         timeout=5*60,
         mode='reschedule',
