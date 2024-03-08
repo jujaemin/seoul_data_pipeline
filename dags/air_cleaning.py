01fcc3f03424b4eaaf9af29c50f014392280e484
jujaemin <147119777+jujaemin@users.noreply.github.com>
2024-03-08 14:45:50 +0900

    Update air_cleaning.py

--- dags/air_cleaning.py
+++ dags/air_cleaning.py
@@ -56,7 +56,7 @@ with DAG(
 
     sensor = ExternalTaskSensor(
         task_id='externaltasksensor',
-        external_dag_id='etl_seoul_air_',
+        external_dag_id='etl_seoul_air__',
         external_task_id='load',
         timeout=5*60,
         mode='reschedule',
