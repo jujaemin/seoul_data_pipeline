4a13009d6a02c64eddf2157b57f2ee18c2dbe538
jujaemin <147119777+jujaemin@users.noreply.github.com>
2024-03-08 14:47:54 +0900

    Update housing_cleaning.py

--- dags/housing_cleaning.py
+++ dags/housing_cleaning.py
@@ -52,7 +52,7 @@ def cleaning():
             pass
 
 with DAG(
-    dag_id = 'jousing_cleaning',
+    dag_id = 'housing_cleaning_',
     start_date = datetime.datetime(2024,1,1),
     max_active_runs = 1,
     catchup = False,
